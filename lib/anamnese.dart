22ad9cb489c11d2f83b7ab284142de24bbdd785a
Pablo <pablossilva.005@gmail.com>
2023-06-02 20:10:21 -0300

    anamnese done

--- lib/anamnese.dart
+++ lib/anamnese.dart
@@ -8,8 +8,7 @@ class Anamnese extends StatefulWidget {
 }
 
 class _AnamneseState extends State<Anamnese> {
-  bool? isChecked = false;
-  bool? isChecked2 = false;
+  bool? isChecked = false, isChecked2 = false, isChecked3 = false;
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -120,6 +119,71 @@ class _AnamneseState extends State<Anamnese> {
                     isChecked = newBool;
                   });
                 }),
+            CheckboxListTile(
+                title: const Text(
+                  'Pulgas',
+                  style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
+                ),
+                value: isChecked2,
+                activeColor: Colors.cyan,
+                onChanged: (newBool) {
+                  setState(() {
+                    isChecked2 = newBool;
+                  });
+                }),
+            CheckboxListTile(
+                title: const Text(
+                  'Carrapatos',
+                  style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
+                ),
+                value: isChecked3,
+                activeColor: Colors.cyan,
+                onChanged: (newBool) {
+                  setState(() {
+                    isChecked3 = newBool;
+                  });
+                }),
+            const ListTile(
+              title: Text(
+                'Vacina',
+                style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
+              ),
+            ),
+            const Padding(
+                padding: EdgeInsets.all(10),
+                child: TextField(
+                  keyboardType: TextInputType.text,
+                  decoration: InputDecoration(
+                    labelText: 'Ex: Antirrábica',
+                    border: OutlineInputBorder(
+                        borderSide: BorderSide(color: Colors.black)),
+                  ),
+                )),
+            const ListTile(
+              title: Text(
+                'Vermifugação',
+                style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
+              ),
+            ),
+            const Padding(
+                padding: EdgeInsets.all(10),
+                child: TextField(
+                  keyboardType: TextInputType.text,
+                  decoration: InputDecoration(
+                    labelText: 'Ex: 15 dias',
+                    border: OutlineInputBorder(
+                        borderSide: BorderSide(color: Colors.black)),
+                  ),
+                )),
+            Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                ElevatedButton(
+                  onPressed: () {},
+                  child: const Text('Download'),
+                )
+              ],
+            )
           ],
         ));
   }
