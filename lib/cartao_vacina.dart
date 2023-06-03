929a8916af59ba9191cb1ce37efa3306188b575f
Pablo <pablossilva.005@gmail.com>
2023-06-02 22:23:21 -0300

    clique cartao_vacina

--- lib/cartao_vacina.dart
+++ lib/cartao_vacina.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:petcare/vacina.dart';
 
 class CartaoVacina extends StatefulWidget {
   const CartaoVacina({super.key});
@@ -24,66 +25,77 @@ class _CartaoVacinaState extends State<CartaoVacina> {
             'Controle de Vacinação',
             style: TextStyle(fontSize: 30, fontWeight: FontWeight.bold),
           ),
-          const ListTile(
-            title: Text(
-              'Vacina:',
-              style: TextStyle(fontSize: 20),
+          GestureDetector(
+            onTap: () {
+              Navigator.of(context).push(
+                MaterialPageRoute(builder: (context) => const Vacina()),
+              );
+            },
+            child: const Column(
+              children: [
+                ListTile(
+                  title: Text(
+                    'Vacina:',
+                    style: TextStyle(fontSize: 20),
+                  ),
+                ),
+                ListTile(
+                  title: Text(
+                    'Dose:',
+                    style: TextStyle(fontSize: 20),
+                  ),
+                ),
+                ListTile(
+                  title: Text(
+                    'Data:',
+                    style: TextStyle(fontSize: 20),
+                  ),
+                ),
+                ListTile(
+                  title: Text(
+                    'Revacina:',
+                    style: TextStyle(fontSize: 20),
+                  ),
+                ),
+                SizedBox(
+                  width: double.infinity,
+                  height: 20,
+                ),
+              ],
             ),
           ),
-          const ListTile(
-            title: Text(
-              'Dose:',
-              style: TextStyle(fontSize: 20),
+            const Text(
+              'Controle de Parasitas',
+              style: TextStyle(fontSize: 30, fontWeight: FontWeight.bold),
             ),
-          ),
-          const ListTile(
-            title: Text(
-              'Data:',
-              style: TextStyle(fontSize: 20),
+            const ListTile(
+              title: Text(
+                'Vermífugo:',
+                style: TextStyle(fontSize: 20),
+              ),
             ),
-          ),
-          const ListTile(
-            title: Text(
-              'Revacina:',
-              style: TextStyle(fontSize: 20),
+            const ListTile(
+              title: Text(
+                'Peso:',
+                style: TextStyle(fontSize: 20),
+              ),
             ),
-          ),
-          const SizedBox(
-            width: double.infinity,
-            height: 20,
-          ),
-          const Text(
-            'Controle de Parasitas',
-            style: TextStyle(fontSize: 30, fontWeight: FontWeight.bold),
-          ),
-          const ListTile(
-            title: Text(
-              'Vermífugo:',
-              style: TextStyle(fontSize: 20),
+            const ListTile(
+              title: Text(
+                'Data:',
+                style: TextStyle(fontSize: 20),
+              ),
             ),
-          ),
-          const ListTile(
-            title: Text(
-              'Peso:',
-              style: TextStyle(fontSize: 20),
+            const ListTile(
+              title: Text(
+                'Revacina:',
+                style: TextStyle(fontSize: 20),
+              ),
             ),
-          ),
-          const ListTile(
-            title: Text(
-              'Data:',
-              style: TextStyle(fontSize: 20),
+            ElevatedButton(
+              onPressed: () {},
+              child: const Text('Download'),
             ),
-          ),
-          const ListTile(
-            title: Text(
-              'Revacina:',
-              style: TextStyle(fontSize: 20),
-            ),
-          ),
-          ElevatedButton(
-            onPressed: () {},
-            child: const Text('Download'),
-          ),
         ],
       ),
     );
---
+++ lib/vacina.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class Vacina extends StatefulWidget {
+  const Vacina({super.key});
+
+  @override
+  State<Vacina> createState() => _VacinaState();
+}
+
+class _VacinaState extends State<Vacina> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
